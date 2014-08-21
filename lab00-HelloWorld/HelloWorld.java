746c2fc5dca5d8cec0cd3f59bba2502429638c14
urk215 <urk215@lehigh.edu>
2014-08-21 15:39:54 +0000

    updated HelloWorld

---
+++ lab00-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+//Urvi Kumar
+//Lab 00 
+//Hello World Test
+
+
+//class definition
+public class HelloWorld{
+    
+    //main method
+    public static void main(String[] args){
+ 
+ //print Hello World with box around it
+ 
+ System.out.println("################");
+ System.out.println("################");
+ System.out.println("# Hello World  #");
+ System.out.println("################");
+ System.out.println("################");
+ 
+ System.out.println("test");
+ 
+ 
+    }
+}
